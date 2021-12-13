4784179663833351a8c5f4af38ca8aae97572cc8
markporo <markporo@yahoo.com>
2021-12-13 01:42:53 -0500

    tests passing, added beforeAll and afterAll functions, also added in jest: testenvironment node in the package.json file

--- api/server.test.js
+++ api/server.test.js
@@ -1,11 +1,30 @@
 const request = require('supertest'); // calling it "request" is a common practice
-const authRouter = require('./auth/auth-router');
-//const server = require('./server')
+//const authRouter = require('./auth/auth-router');
+const server = require('./server')
+const db = require("../data/dbConfig");
+
+
 
 // Write your tests here
 test('sanity', () => {
-  expect(true).toBe(true)
+  expect(true).not.toBe(false)
 })
+
+// it("correct env var", () => {
+//   expect(process.env.DB_ENV).toBe("testing")
+// })
+
+beforeAll(async () => {
+  // await db('test').truncate()
+  await db.migrate.rollback()
+  await db.migrate.latest()
+})
+
+afterAll(async () => {
+  await db.destroy()
+})
+
+
 
 describe('auth endpoints', () => {
   // http calls made with supertest return promises, we can use async/await if desired
@@ -14,18 +33,18 @@ describe('auth endpoints', () => {
       const expectedStatusCode = 201;
 
       // do a post request to our api (server.js) and inspect the response
-      const response = await request(authRouter).post('/register').send({
+      const response = await request(server).post('/api/auth/register').send({
         "username": "Captain Marvel",
         "password": "foobar"
       });
 
       expect(response.statusCode).toBe(expectedStatusCode)
 
-    }, 20000);
+    });
 
 
     it('should respond with a new user id', async () => {
-      const response = await request(authRouter).post('/register').send({
+      const response = await request(server).post('/api/auth/register').send({
         "username": "Captain Marvel",
         "password": "foobar"
       });
@@ -51,16 +70,17 @@ describe('auth endpoints', () => {
   });
 
   describe("login route", () => {
-    it("should respond with a status code of 400", async () => {
-      const response = await request(authRouter).post("/login").send({
+    it("should respond with a status code of 401 due to invalid credentials", async () => {
+      const response = await request(server).post("/api/auth/login").send({
+        "username": "Captain Marvellllll",
         "password": "foobar"
       });
 
-      expect(response.statusCode).toBe(400)
+      expect(response.statusCode).toBe(401)
     })
 
     it('should return a JSON object from the login route', async () => {
-      const response = await request(authRouter).post('/login').send({
+      const response = await request(server).post('/api/auth/login').send({
         "username": "Captain Marvel",
         "password": "foobar"
       });
