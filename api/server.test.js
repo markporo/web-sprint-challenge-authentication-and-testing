8d0ff8fd9a7aca1733a98db3605719b2d6e012dd
markporo <markporo@yahoo.com>
2021-12-13 02:35:12 -0500

    added tests, all passing

--- api/server.test.js
+++ api/server.test.js
@@ -1,5 +1,5 @@
 const request = require('supertest'); // calling it "request" is a common practice
-//const authRouter = require('./auth/auth-router');
+//const authRouter = require('./auth/auth-router');// ----doesn't work this way
 const server = require('./server')
 const db = require("../data/dbConfig");
 
@@ -27,8 +27,17 @@ afterAll(async () => {
 
 
 describe('auth endpoints', () => {
+
+
+
   // http calls made with supertest return promises, we can use async/await if desired
   describe('register route', () => {
+
+    beforeEach(async () => {
+      await db('users').truncate()
+
+    })
+
     it('should return an 201 status code from the register route', async () => {
       const expectedStatusCode = 201;
 
@@ -37,10 +46,20 @@ describe('auth endpoints', () => {
         "username": "Captain Marvel",
         "password": "foobar"
       });
-
+      expect(response.body).toHaveProperty("id");
+      expect(response.body).toHaveProperty("username");
+      expect(response.body).toHaveProperty("password");
       expect(response.statusCode).toBe(expectedStatusCode)
 
     });
+
+    it("fails upon username missing", async () => {
+      const response = await request(server)
+        .post("/api/auth/register")
+        .send({ "password": "foobar" })
+
+      expect(response.body.message).toBe("username and password required")
+    })
 
 
     it('should respond with a new user id', async () => {
@@ -70,6 +89,13 @@ describe('auth endpoints', () => {
   });
 
   describe("login route", () => {
+
+    beforeEach(async () => {
+      await db('users').truncate()
+
+    })
+
+
     it("should respond with a status code of 401 due to invalid credentials", async () => {
       const response = await request(server).post("/api/auth/login").send({
         "username": "Captain Marvellllll",
@@ -79,15 +105,31 @@ describe('auth endpoints', () => {
       expect(response.statusCode).toBe(401)
     })
 
-    it('should return a JSON object from the login route', async () => {
-      const response = await request(server).post('/api/auth/login').send({
+    it('should login a registered user', async () => {
+      const response = await request(server).post('/api/auth/register').send({
         "username": "Captain Marvel",
         "password": "foobar"
       });
 
-      expect(response.statusCode).toBe(200)
+      const response2 = await request(server).post('/api/auth/login').send({
+        "username": "Captain Marvel",
+        "password": "foobar"
+      });
+      expect(response.statusCode).toBe(201) // registered
+      expect(response2.statusCode).toBe(200) // logged in
+      expect(response2.body).toHaveProperty("message");
+      expect(response2.body).toHaveProperty("token");
+
       //expect(response.headers['content-type']).toEqual(expect.stringContaining("json"));
     });
+
+    it("will not log in a user without registering first", async () => {
+      const response = await request(server).post('/api/auth/login').send({
+        "username": "Captain Marvel",
+        "password": "foobar"
+      })
+      expect(response.body).not.toHaveProperty("token")
+    })
 
   })
 });
